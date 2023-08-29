03227ad56222ec2ed0d9ff3fd19c46c9f6bf4caf
guiroveri <guiroverii@gmail.com>
2023-08-29 20:47:06 -0300

    iniciando configurações do microserviço

---
+++ login-ms/login-ms/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Presentation.Application.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ login-ms/login-ms/Startup.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Presentation.Application.Extensions;
+using System.Text;
+
+namespace Presentation.Application
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddAuthentication
+            //     (JwtBearerDefaults.AuthenticationScheme)
+            //     .AddJwtBearer(options =>
+            //     {
+            //         options.TokenValidationParameters = new TokenValidationParameters
+            //         {
+            //             ValidateIssuer = true,
+            //             ValidateAudience = true,
+            //             ValidateLifetime = true,
+            //             ValidateIssuerSigningKey = true,
+
+            //             ValidIssuer = Configuration["Jwt:Issuer"],
+            //             ValidAudience = Configuration["Jwt:Audience"],
+            //             IssuerSigningKey = new SymmetricSecurityKey
+            //           (Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+            //         };
+            //     });
+
+            services.AddVersionedApiExplorer(
+                options =>
+                {
+                    options.GroupNameFormat = "'v'VVV";
+                    options.SubstituteApiVersionInUrl = true;
+                });
+
+            services.AddControllers();
+            services.AddApiVersioning(o => o.ReportApiVersions = true);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "login-ms", Version = "v1" });
+            });
+
+            services.AddDependencies();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "login-ms v1"));
+            }
+
+           // app.UseAuthentication();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
