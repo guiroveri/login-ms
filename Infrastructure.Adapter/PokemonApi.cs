3667684f4e28d4bb899ac3f3e7b3b77b2b0c90c2
guiroveri <guiroverii@gmail.com>
2023-09-14 20:55:48 -0300

    testando integração de APIs

---
+++ Infrastructure.Adapter/PokemonApi.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Adapter.Model;
+using Newtonsoft.Json;
+
+namespace Infrastructure.Adapter
+{
+    public class PokemonApi
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseurl = "https://pokeapi.co/api/v2/";
+        
+        public PokemonApi(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<PokemonStatus> PokemonInformation(string name)
+        {
+            var url = $"{_baseurl}pokemon/{name.ToLower()}";
+
+            var response = await _httpClient.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            var responsebody = await response.Content.ReadAsStringAsync();
+            var information = JsonConvert.DeserializeObject<PokemonStatus>(responsebody);
+
+            return information;
+        }
+
+    }
+}
